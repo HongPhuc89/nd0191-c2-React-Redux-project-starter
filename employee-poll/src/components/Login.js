b42b2dd526bd13025fb8c906b8a1183031a03e2c
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-10-18 17:08:45 +0700

    Add login logo

--- employee-poll/src/components/Login.js
+++ employee-poll/src/components/Login.js
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { useState } from "react";
 import { handleLogin } from "../actions/authenticateUser";
+import loginLogo from "./../asserts/images/loginLogo.png";
 
 const Login = ({ dispatch, loggedIn }) => {
   const [username, setUsername] = useState("");
@@ -31,38 +32,55 @@ const Login = ({ dispatch, loggedIn }) => {
   };
 
   return (
-    <div>
-      <h1 data-testid="login-heading">Employee Poll</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <div>
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-black-700">
+          Employee Polls
+        </h1>
+        <img src={loginLogo} alt="login logo"/>
+        <h1 className="text-3xl font-semibold text-center text-blue-700 underline mt-3">
+          Sign in
+        </h1>
+        <form className="mt-6" onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label
+              for="email"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Username
+            </label>
             <input
               value={username}
               onChange={handleUsername}
               type="text"
               name="username"
               id="username"
-              data-testid="username"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <div>
+          <div className="mb-2">
+            <label
+              for="password"
+              className="block text-sm font-semibold text-gray-800"
+            >
+              Password
+            </label>
             <input
               value={password}
               onChange={handlePassword}
               type="password"
               name="password"
               id="password"
+              className="block w-full px-4 py-2 mt-2 text-blue-700 bg-white border rounded-md focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
             />
           </div>
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
+          <div className="mt-6">
+            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-700 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
