f810d0aaf97bfe4c0cf23f4b0bc331b3bf1dbc56
Nguyen Hong Phuc <scottfu89@gmail.com>
2023-10-20 16:29:40 +0700

    Update question list

--- employee-poll/src/components/QuestionList.js
+++ employee-poll/src/components/QuestionList.js
@@ -3,16 +3,22 @@ import Card from "./Card";
 
 const QuestionList = ({ title, questions, users, questionFilter }) => {
   return (
-    <div className="mt-8">
-      <h2 className="text-2xl font-bold mt-6">{title}</h2>
+    <div className="mt-8 border">
+      <div className="flex justify-center border">
+        <div className="my-4">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        </div>
+      </div>
 
-      <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {questions.filter(questionFilter).map((question) => (
-          <li key={question.id}>
-            <Card question={question} author={users[question.author]} />
-          </li>
-        ))}
-      </ul>
+      <div>
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          {questions.filter(questionFilter).map((question) => (
+            <li key={question.id}>
+              <Card question={question} author={users[question.author]} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
