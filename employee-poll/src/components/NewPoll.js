3fb15141cc1a04232fb1f39c74a97f7c93daed3f
Hong Phuc Nguyen <scottfu89@gmail.com>
2023-10-20 22:01:01 +0700

    Update New Poll testcase

--- employee-poll/src/components/NewPoll.js
+++ employee-poll/src/components/NewPoll.js
@@ -48,6 +48,7 @@ const NewPoll = ({ dispatch }) => {
               type="text"
               name="firstOption"
               id="firstOption"
+              data-testid="firstOption"
               className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
             />
           </div>
@@ -56,6 +57,7 @@ const NewPoll = ({ dispatch }) => {
         <div className="mt-3">
           <label
             htmlFor="secondOption"
+            data-testid="secondOptionLabel"
             className="block text-sm font-medium text-slate-700"
           >
             Second Option
@@ -67,6 +69,7 @@ const NewPoll = ({ dispatch }) => {
               type="text"
               name="secondOption"
               id="secondOption"
+              data-testid="secondOption"
               className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400  disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
             />
           </div>
@@ -75,6 +78,7 @@ const NewPoll = ({ dispatch }) => {
         <div className="mt-6 text-center">
           <button
             type="submit"
+            data-testid="submit-poll"
             disabled={isSubmitDisabled}
 
             className={`${
